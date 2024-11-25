0edad63451cebc66eece8e1fd5c1f09229923776
huy37204 <huy37204@gmail.com>
2024-11-25 15:17:45 +0700

    cấu trúc lại thư mục view

--- index.js
+++ index.js
@@ -8,47 +8,41 @@ app.set("views", "./views");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home", { activePage: "home" });
+  res.render("customer/home", { activePage: "home" });
 });
 
 app.get("/branch-home", (req, res) => {
-  res.render("branch_home");
+  res.render("branch/branch_home");
 });
 
 app.get("/branch-employee-list", (req, res) => {
-  res.render("branch_employee_list");
+  res.render("branch/branch_employee_list");
 });
 
 app.get("/branch-employee-search", (req, res) => {
-  res.render("branch_employee_search");
+  res.render("branch/branch_employee_search");
 });
 
 app.get("/branch-order-form", (req, res) => {
-  res.render("branch_order_form");
+  res.render("branch/branch_order_form");
 });
 
 app.get("/branch-invoice", (req, res) => {
-  res.render("branch_invoice");
+  res.render("branch/branch_invoice");
 });
 
 app.get("/branch-customer-card", (req, res) => {
-  res.render("branch_customer_card");
+  res.render("branch/branch_customer_card");
 });
 
 app.get("/dat-ban", (req, res) => {
-  res.render("table_booking", { activePage: "table_booking" });
+  res.render("customer/table_booking", { activePage: "table_booking" });
 });
 
 app.get("/menu", (req, res) => {
-  res.render("menu", { activePage: "menu" });
+  res.render("customer/menu", { activePage: "menu" });
 });
 
 app.listen(port, () => {
   console.log(`Project dang chay o port ${port}`);
 });
-
-console.log("a");
-console.log("a");
-console.log("a");
-console.log("a");
-console.log("a");
