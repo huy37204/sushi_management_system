140708407e7b6286338f2a165a2f3103374281e2
huy37204 <150261892+huy37204@users.noreply.github.com>
2024-11-27 15:48:04 +0700

    Huy branch view (#3)
    * test
    
    * cấu trúc lại thư mục view
    
    * Thêm view company
    
    * add div menu
    
    ---------
    
    Co-authored-by: Khoi Pham <dinhkhoi26031997@gmail.com>

--- index.js
+++ index.js
@@ -8,43 +8,54 @@ app.set("views", "./views");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home", { activePage: "home" });
+  res.render("customer/home", { activePage: "home" });
+});
+app.get("/signin", (req, res) => {
+  res.render("signin", { activePage: "signin" });
 });
 
 app.get("/branch-home", (req, res) => {
-  res.render("branch_home");
+  res.render("branch/branch_home");
 });
 
 app.get("/branch-employee-list", (req, res) => {
-  res.render("branch_employee_list");
+  res.render("branch/branch_employee_list");
 });
 
 app.get("/branch-employee-search", (req, res) => {
-  res.render("branch_employee_search");
+  res.render("branch/branch_employee_search");
 });
 
 app.get("/branch-order-form", (req, res) => {
-  res.render("branch_order_form");
+  res.render("branch/branch_order_form");
 });
 
 app.get("/branch-invoice", (req, res) => {
-  res.render("branch_invoice");
+  res.render("branch/branch_invoice");
 });
 
 app.get("/branch-customer-card", (req, res) => {
-  res.render("branch_customer_card");
+  res.render("branch/branch_customer_card");
+});
+
+app.get("/company-home", (req, res) => {
+  res.render("company/company_home");
+});
+
+app.get("/company-food-revenue", (req, res) => {
+  res.render("company/company_food_revenue");
 });
 
 app.get("/dat-ban", (req, res) => {
-  res.render("table_booking", { activePage: "table_booking" });
+  res.render("customer/table_booking", { activePage: "table_booking" });
 });
 
 app.get("/menu", (req, res) => {
-  res.render("menu", { activePage: "menu" });
+  res.render("customer/menu", { activePage: "menu" });
 });
-
+app.get("/cart", (req, res) => {
+  res.render("cart", { activePage: "cart" });
+});
 app.listen(port, () => {
   console.log(`Project dang chay o port ${port}`);
 });
-
-console.log("a");
